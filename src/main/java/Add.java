a80556cd6f3fa802d6cfe8787d1c6125e38b65d7
vishnu giri <vgvishnugiri@gmail.com>
2018-09-28 19:43:02 -0400

    New Class Add.

---
+++ src/main/java/Add.java
@@ -0,0 +1,12 @@
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=10;
+int c=a+b;
+System.out.println("Adding two numbers"+ " "+c);
+	}
+
+}
